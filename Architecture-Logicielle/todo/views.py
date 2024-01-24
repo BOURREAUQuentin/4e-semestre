e7b57bedbacdc06eb487be89164b64acd706ec9b
Bourreau Quentin <o22202632@info23-14.iut45.univ-orleans.fr>
2024-01-24 18:26:27 +0100

    PUT et debut DELETE

--- Architecture-Logicielle/todo/views.py
+++ Architecture-Logicielle/todo/views.py
@@ -2,7 +2,7 @@ from flask import jsonify, abort, make_response, request, url_for
 from .app import app
 from .models import tasks
 
-def make_public_tasks(task):
+def make_public_task(task):
     new_task = {}
     for field in task:
         if field == "id":
@@ -13,7 +13,7 @@ def make_public_tasks(task):
 
 @app.route("/todo/api/v1.0/tasks", methods = ["GET"])
 def get_tasks():
-    return jsonify(tasks = [make_public_tasks(t) for t in tasks])
+    return jsonify(tasks = [make_public_task(t) for t in tasks])
 
 @app.route("/todo/api/v1.0/tasks/<int:task_id>", methods = ["GET"])
 def get_task(task_id):
@@ -41,7 +41,7 @@ def create_task():
         "done": False
     }
     tasks.append(task)
-    return jsonify({"task": make_public_tasks(task)}), 201
+    return jsonify({"task": make_public_task(task)}), 201
 
 @app.route("/todo/api/v1.0/tasks/<int:task_id>", methods=["PUT"])
 def update_task(task_id):
@@ -52,4 +52,20 @@ def update_task(task_id):
         abort(400)
     if "title" in request.json and type(request.json["title"]) != str:
         abort(400)
-    if "description" in request
+    if "description" in request.json and type(request.json["description"]) is not str:
+        abort(400)
+    if "done" in request.json and type(request.json["done"]) is not bool:
+        abort(400)
+    task[0]["title"] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': make_public_task(task[0])})
+
+@app.route("/todo/api/v1.0/tasks/<int:task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = [task for task in tasks if task["id"] == task_id]
+    print(len(task))
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'task deleted'})

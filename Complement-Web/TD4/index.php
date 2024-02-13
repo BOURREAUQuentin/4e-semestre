0d72bf90ec564e4524f1cfa021a35b4e36689d58
Bourreau Quentin <o22202632@info07-11.iut45.univ-orleans.fr>
2024-02-13 12:17:22 +0100

    td4 complement web

---
+++ Complement-Web/TD4/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo modules JS</title>
+</head>
+<body>
+    <p class="design">Test des modules en JS</p>
+    <div id="result"></div>
+</body>
+</html>
+<script type="module" src="main.js"></script>
---
+++ Complement-Web/TD4/main.js
@@ -0,0 +1,4 @@
+import { add, multiply } from './util.js';
+
+console.log(add(2, 3)); // Output: 5
+console.log(multiply(2, 3)); // Output: 6

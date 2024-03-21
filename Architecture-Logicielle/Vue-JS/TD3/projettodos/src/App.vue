80ad1330827364555e037c057322af394a2dfb54
Bourreau Quentin <o22202632@info24-12.iut45.univ-orleans.fr>
2024-03-21 08:20:41 +0100

    feat: vue-js td3 delete tasks

---
+++ Architecture-Logicielle/Vue-JS/TD3/projettodos/src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+let data = {
+  todos:[
+    { id: 1, text: 'Apprendre ES6', checked: true},
+    { id: 2, text: 'Apprendre Vue', checked: false},
+    { id: 3, text: 'Faire un projet magnifique', checked: false},
+  ]
+};
+
+export default {
+  data(){
+    return data;
+  },
+  components: { TodoItem },
+  methods: {
+    removeItem(todo) {
+      this.todos = this.todos.filter(t => t.id !== todo.id);
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>Liste de taches</h2>
+  <TodoItem v-for="tache of todos" :todo="tache" :key="tache.id" @remove="removeItem"></TodoItem>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ Architecture-Logicielle/Vue-JS/TD3/projettodos/src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default{
+    props: {
+        todo: Object
+    },
+    methods: {
+        suppr: function(){
+            this.$emit("remove", {id:this.todo.id});
+        }
+    },
+    emits : ["remove"]
+}
+</script>
+<template>
+    <div class="checkbox">
+        <label>
+            <input type="checkbox" checked="checked" v-model="todo.checked">
+            {{ todo.text}}
+        </label>
+        <input type="button"
+        class="btn btn-danger"
+        value="Supprimer"
+        @click="suppr">
+    </div>
+</template>

f462792b4cfbde91dee4523b310f02429be4b554
Quentin_BOURREAU <quentin.bourreau@etu.univ-orleans.fr>
2024-03-23 15:29:07 +0100

    modification et ajout d'un todo avec VueJS

--- Architecture-Logicielle/Vue-JS/TD3/projettodos/src/App.vue
+++ Architecture-Logicielle/Vue-JS/TD3/projettodos/src/App.vue
@@ -6,7 +6,8 @@ let data = {
     { id: 1, text: 'Apprendre ES6', checked: true},
     { id: 2, text: 'Apprendre Vue', checked: false},
     { id: 3, text: 'Faire un projet magnifique', checked: false},
-  ]
+  ],
+  addModalVisible: false
 };
 
 export default {
@@ -17,6 +18,22 @@ export default {
   methods: {
     removeItem(todo) {
       this.todos = this.todos.filter(t => t.id !== todo.id);
+    },
+    showAddTodo(){
+      this.addModalVisible = !this.addModalVisible;
+      this.newText = '';
+    },
+    addTodo(newText) {
+      let maxId = 0;
+      for (const todo of this.todos) {
+        if (todo.id > maxId) {
+          maxId = todo.id;
+        }
+      }
+      const newTodo = { id: maxId + 1, text: newText, checked: false };
+      this.todos.push(newTodo);
+      this.showAddTodo();
+      console.log(this.todos);
     }
   }
 }
@@ -25,6 +42,18 @@ export default {
 <template>
   <h2>Liste de taches</h2>
   <TodoItem v-for="tache of todos" :todo="tache" :key="tache.id" @remove="removeItem"></TodoItem>
+
+  <input type="button"
+    class="btn"
+    value="Ajouter une tâche"
+    @click="showAddTodo">
+  <div v-if="addModalVisible">
+    <input type="button" class="btn" value="Annuler" @click="showAddTodo">
+    <form @submit.prevent="addTodo(newText)">
+      <input type="text" v-model="newText" required>
+      <input type="submit" value="Enregistrer">
+    </form>
+  </div>
 </template>
 
 <style scoped>
--- Architecture-Logicielle/Vue-JS/TD3/projettodos/src/components/TodoItem.vue
+++ Architecture-Logicielle/Vue-JS/TD3/projettodos/src/components/TodoItem.vue
@@ -4,12 +4,35 @@ export default{
     props: {
         todo: Object
     },
+    data() {
+        return {
+            editModalVisible: false,
+            addModalVisible: false,
+            newText: ''
+        };
+    },
     methods: {
         suppr: function(){
             this.$emit("remove", {id:this.todo.id});
+        },
+        showEditModal: function(){
+            this.editModalVisible = ! this.editModalVisible;
+        },
+        editTodo: function(newText){
+            this.todo.text = newText;
+            this.editModalVisible = false;
+            this.newText = "";
+            // est-ce que faut faire un emit pour modifier au parent (serveur) ???
+            //this.$emit("edit", {id:this.todo.id, newText: newText});
+        },
+        showAddModal: function(){
+            this.addModalVisible = !this.addModalVisible;
+        },
+        addTodo: function(newText){
+            this.$emit("add", {text: newText});
         }
     },
-    emits : ["remove"]
+    emits : ["remove", "add"]
 }
 </script>
 <template>
@@ -18,9 +41,27 @@ export default{
             <input type="checkbox" checked="checked" v-model="todo.checked">
             {{ todo.text}}
         </label>
+
         <input type="button"
         class="btn btn-danger"
         value="Supprimer"
         @click="suppr">
+
+        <input type="button"
+        class="btn"
+        value="Modifier"
+        @click="showEditModal">
+
+        <div class="alert alert-success" v-if="todo.checked">
+            Done
+        </div>
+
+        <div v-if="editModalVisible">
+            <input type="button" class="btn" value="Annuler" @click="showEditModal">
+            <form @submit.prevent="editTodo(newText)">
+                <input type="text" v-model="newText" required>
+                <input type="submit" value="Enregistrer">
+            </form>
+        </div>
     </div>
 </template>
